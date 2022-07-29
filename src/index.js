888ee011a90c3e8a1e89895e62cad79e2fb41bf5
fullgon <mbj0119@gmail.com>
2022-07-29 11:27:37 +0900

    ADD: styled-component를 사용하여  styling하기

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SassComponent />
+    <StyledComponent />
   </React.StrictMode>
 );
